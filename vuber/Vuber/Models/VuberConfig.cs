0a221a0ade3f89c2591cd98f5c9b82e9b8e83522
Kemal Karadag <kemal.karadag@bilgi.edu.tr>
2016-08-31 12:49:56 +0300

    initial

---
+++ vuber/Vuber/Models/VuberConfig.cs
@@ -0,0 +1,99 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vuber.Models
+{
+
+
+
+
+
+    public class VuberConfig
+    {
+        public string ConnectionString { get; set; }
+        public string Directory { get; set; }
+        public string ExecutedFolder { get; set; }
+        public string RollBackFolder { get; set; }
+
+
+
+        public VuberConfig(string initial)
+        {
+            string ConfigFile = string.Format(@"{0}\config.json", Environment.CurrentDirectory);
+            if (!File.Exists(ConfigFile))
+            {
+                SaveConfigFile(this);
+            }
+
+            VuberConfig initialObject = new VuberConfig();
+            initialObject = JsonConvert.DeserializeObject<VuberConfig>(File.ReadAllText(ConfigFile));
+
+            this.ConnectionString = initialObject.ConnectionString;
+           
+
+
+        }
+
+        public VuberConfig()
+        { }
+
+
+
+
+
+    //    //public VuberConfig()
+    //    //{            
+    //    //    //if (!isConfigFileExisist())
+    //    //    //{
+    //    //    //    this.SaveConfigFile(this);
+    //    //    //}
+    //    //    //string ConfigFile = string.Format(@"{0}\config.json", Environment.CurrentDirectory);
+
+    //    //    //if (!File.Exists(ConfigFile))
+    //    //    //{
+    //    //    //    this.SaveConfigFile(this);
+    //    //    //}
+            
+
+    //    //    //VuberConfig obj =  JsonConvert.DeserializeObject<VuberConfig>(File.ReadAllText(ConfigFile));
+            
+    //    //    //this.ConnectionString = obj.ConnectionString;
+    //    //    //this.Directory = obj.Directory;
+    //    //    //this.ExecutedFolder = obj.ExecutedFolder;
+    //    //    //this.RollBackFolder = obj.RollBackFolder;
+    //    //}
+
+        
+
+
+    //    public bool isConfigFileExisist()
+    //    {
+    //        string ConfigFile = string.Format(@"{0}\config.json", Environment.CurrentDirectory);
+
+    //        if (File.Exists(ConfigFile))
+    //        {
+    //            return true;
+    //        }
+    //        else
+    //        {
+    //            return false;
+    //        }
+    //    }
+        public void SaveConfigFile(VuberConfig obj)
+        {
+            string ConfigFile = string.Format(@"{0}\config.json", Environment.CurrentDirectory);
+            string output = JsonConvert.SerializeObject(obj);
+            File.WriteAllText(ConfigFile, output);
+        }
+
+        
+
+
+    }
+}
---
+++ vuber/Vuber/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vuber.Models;
+
+namespace Vuber
+{
+    class Program
+    {
+
+       
+
+        static void Main(string[] args)
+        {
+            VuberConfig config = new VuberConfig("inital");
+
+            Console.WriteLine(config.ConnectionString);
+            Console.ReadKey();
+
+        }
+    }
+}
