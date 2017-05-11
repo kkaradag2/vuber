747af894a855f145f9acf30a58d69bdcdb2b99b1
kemal.karadag@bilgi.edu.tr <kemal.karadag@bilgi.edu.tr>
2017-05-11 15:31:03 +0300

    Model changed

--- vuber/Vuber/Models/migration.cs
+++ vuber/Vuber/Models/migration.cs
@@ -5,5 +5,11 @@ namespace Vuber.Models
     [Verb("migrate", HelpText = "Migrate pending files to database.")]
     internal class Migration
     {
+        [Option('m', "Migrate Special Folder", HelpText = "Migrate only entered folder. Sample: vuber migrate -m DPL-100")]
+        public string MigrateDirectory { get; set; }
+
+        [Option('e', "Exculute Special Folder to migrate", HelpText = "Exclude declareted folder to migrate. Sample: vuver migrate -e DPL-105")]
+        public string ExculudeDirectory { get; set; }
+
     }
 }
